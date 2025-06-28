fd0a5fd847867dadcf36f3577d9dfaf7667ff225
Iona-401 <sidlipika@gmail.com>
2025-06-29 01:05:04 +0400

    Update main.py

--- Heart Disease/main.py
+++ Heart Disease/main.py
@@ -8,6 +8,23 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-data = pd.read_csv("dataset.cev")
+data = pd.read_csv("Heart Disease\dataset.csv")
 print(data.head())
 print(data["target"].value_counts())
+
+X = data.drop("target", axis=1)
+y = data["target"]
+
+scaler = StandardScaler()
+scaled_X = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred))
